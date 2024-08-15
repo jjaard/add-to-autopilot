922f7edbe3ad840aab4e6651518f72950b917bba
ЙAARD <114291676+jjaard@users.noreply.github.com>
2024-08-15 16:40:45 +0300

    Update for-powershell.sh

--- for-powershell.sh
+++ for-powershell.sh
@@ -1,4 +1,4 @@
 Install-Script -Name Get-WindowsAutopilotInfo
 Set-ExecutionPolicy Bypass
 Get-WindowsAutoPilotInfo.ps1 -OutputFile C:\Device-info.csv
-Start-Process msedge
+Start-Process msedge "https://intune.microsoft.com/#view/Microsoft_Intune_Enrollment/AutopilotDevices.ReactView/filterOnManualRemediationRequired~/false"
